c7cde1c17c2ef677b1db7377bdafe340088efe90
giamann1 <145729311+giamann1@users.noreply.github.com>
2024-06-16 22:19:25 -0400

    Add files via upload

---
+++ P1LAB1_ManningGia.py
@@ -0,0 +1,9 @@
+# Prompt the user to enter their first name
+first_name = input("Please enter your first name: ")
+
+# Prompt the user to enter their last name
+last_name = input("Please enter your last name: ")
+
+# Display a welcome message to the user
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")
+
